b29fa9f13743f28956569842a56553a34e159cef
Jonatan Missora <jonatanjmissora@gmail.com>
2025-05-20 16:04:23 -0300

    blue background login fix

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   if (user) redirect("/pendientes")
 
   return (
-    <section className="h-[95dvh] flex justify-center items-center relative bg-blue-400">
+    <section className="h-[95dvh] flex justify-center items-center relative">
 
       <LoginForm />
 
